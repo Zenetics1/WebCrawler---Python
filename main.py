b96933694c94a5e8cf04aa994b10e37c9a09fafd
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2024-07-31 11:42:02 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from bs4 import BeautifulSoup
+import requests
 
-linkInput = input("Enter Your Link: ")
+URLInput = "https://www.newegg.ca/amd-ryzen-7-7800x3d-ryzen-7-7000-series/p/N82E16819113793"
+#input("Enter Your Link: ")
 
-with open(linkInput) as webLink:
-    link = BeautifulSoup(webLink)
-    print(link)
+result = requests.get(URLInput)
+
+with open(result) as link:
+    page = BeautifulSoup(link.text, "html.parser")
+    print(page.prettify)
